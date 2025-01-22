40f22693694d86846e199ae857fd9bc8404ea09f
cp-22rel <reto.lehmann@ceff.ch>
2025-01-22 14:15:01 +0100

    Resolved UI bug where endgame gets cancelet

--- src/ui_manager.mjs
+++ src/ui_manager.mjs
@@ -3,19 +3,23 @@ import { InformationPanelUI } from "./ui/information_panel_ui.mjs";
 import { PauseMenuUI } from "./ui/pause_menu_ui.mjs";
 
 class UIManager {
-    constructor() { };
+    constructor() {
+        this.ui = null;
+    };
 
     static manager = new UIManager();
 
     showUI(ui, param, map) {
-        map.currentGameState = "paused";
-        switch (ui) {
-            case "endGameUI":
-                this.ui = new EndGameUI(param);
-                break;
-            case "informationPanelUI": this.ui = new InformationPanelUI(param); break;
-            case "pauseMenuUI": this.ui = new PauseMenuUI(param); break;
-            default: break;
+        if (this.ui === null) {
+            map.currentGameState = "paused";
+            switch (ui) {
+                case "endGameUI":
+                    this.ui = new EndGameUI(param);
+                    break;
+                case "informationPanelUI": this.ui = new InformationPanelUI(param); break;
+                case "pauseMenuUI": this.ui = new PauseMenuUI(param); break;
+                default: break;
+            }
         }
     }
 
